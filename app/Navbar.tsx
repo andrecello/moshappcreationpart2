9a8a59afadab4af17947df1b993acbfc63cbee2b
andrecello <143760719+andrecello@users.noreply.github.com>
2025-03-12 13:28:21 -1000

    ActiveLinkbefore
    Commiting before I work on the activelink. May script if mosh's verision is too version dependent.

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,16 +1,30 @@
+'use client'
 import Link from 'next/link'
 import React from 'react'
 import { BiSolidGame } from "react-icons/bi";
+import { usePathname } from 'next/navigation'
 
 const Navbar = () => {
+    const currentPath = usePathname();
+    console.log(currentPath);
+    
+    const links = [
+        { label: 'Dashboard', href: '/'},
+        { label: 'Issues', href: '/issues'},
+        { label: 'About me', href: '/aboutme'},
+        { label: 'Troubleshooting', href:'/troubleshooting'},
+        { label: 'Blog', href:'/blog'}
+
+    ]
   return (
     <nav className = 'flex space-x-6 border mb-5 px-5 h-14 items-center'>
         <Link href='/'><BiSolidGame /></Link>
         <ul className = 'flex space-x-6'>
-            <li><Link className = 'text-sky-500 hover:text-sky-950' href='/'>Dashboard</Link></li>
-            <li><Link href='/issues'>Issues</Link></li>
-            <li><Link href='/aboutme'>About Me</Link></li>
-            <li><Link href='/welcomescreen'>TroubleShooting</Link></li>
+            {links.map(link => 
+                <Link 
+                    key={link.href}
+                        className = 'text-sky-500 hover:text-sky-950 transition-colors' 
+                        href={link.href}>{link.label}</Link>)}
         </ul>
     </nav>
   )
